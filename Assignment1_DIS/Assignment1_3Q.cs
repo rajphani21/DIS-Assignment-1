87dc2f5eba1f78fda1ea69a5023c0c3d3f406fa4
rajphani21 <kakarla24@usf.edu>
2021-06-03 18:16:01 -0400

    3Q_Assignment1

---
+++ Assignment1_DIS/Assignment1_3Q.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment1_Summer2021
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Q6: Enter the sentence to convert:");
+			string sentence = Console.ReadLine();
+			string goatLatin = ToGoatLatin(sentence);
+			Console.WriteLine("Goat Latin for the Given Sentence is {0}", goatLatin);
+			Console.WriteLine();
+		}
+		public static string ToGoatLatin(string sentence)
+		{
+			String[] words = sentence.Split(' ');
+			string modified_sentence = "";
+			//Console.WriteLine(words.Length)
+			int iteration_number = 0;
+			foreach (string word in words)
+			{
+				//Console.WriteLine(word.Length);
+				if (word[0].ToString().Equals("a", StringComparison.OrdinalIgnoreCase) || word[0].ToString().Equals("e", StringComparison.OrdinalIgnoreCase) || word[0].ToString().Equals("i", StringComparison.OrdinalIgnoreCase) || word[0].ToString().Equals("o", StringComparison.OrdinalIgnoreCase) || word[0].ToString().Equals("u", StringComparison.OrdinalIgnoreCase))
+				//if(word[0].ToString() == "a" || word[0].ToString() == "e" || word[0].ToString() == "i" || word[0].ToString() == "o" || word[0].ToString() == "u")
+				{
+					modified_sentence = modified_sentence + " " + word + "ma";
+				}
+				else
+				{
+					modified_sentence = modified_sentence + " " + word.Substring(1) + word[0] + "ma";
+				}
+				iteration_number++;
+				for (int i = 0; i < iteration_number; i++)
+				{
+					modified_sentence = modified_sentence + "a";
+				}
+			}
+			Console.WriteLine(iteration_number);
+			return modified_sentence.Substring(1);
+		}
+	}
+}
